d0a4548ae67dd4cb1759c187c5f8e1bc46ff7b07
William Witteman <william.witteman.cisssca@ssss.gouv.qc.ca>
2021-07-26 16:03:44 -0400

    Adding a parameter file and structure

--- mockData.py
+++ mockData.py
@@ -6,5 +6,9 @@ written into a CSV file."""
 
 import pandas
 import numpy
+import random
 
+# Read the parameters.csv file into a dataframe
+parameters = pandas.read_csv("parameters.csv" , index_col=0)
 
+print(parameters)
