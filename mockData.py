8bcfad341c3988ad12087c5495178bab212a58e2
William Witteman <william.witteman.cisssca@ssss.gouv.qc.ca>
2021-08-09 16:29:12 -0400

    Starting to gel.  Using Faker for key pieces.

--- mockData.py
+++ mockData.py
@@ -5,10 +5,38 @@ test your analyses on. Parameters are written in a CSV file, mock data is
 written into a CSV file."""
 
 import pandas
-import numpy
 import random
+import faker
 
-# Read the parameters.csv file into a dataframe
-parameters = pandas.read_csv("parameters.csv" , index_col=0)
+def integer(name , start , end):
+    number = random.randint( start , end)
+    return (name , number)
 
-print(parameters)
+def factor(name , factors):
+    return (name , random.choice(factors))
+
+def string(name , length):
+    fake = faker.Faker()
+    return (name , fake.text(length))
+
+
+def mockData(parameterfile , count , filename = None):
+    if filename == None:
+        filename = "mockdata.csv"
+    else:
+        pass
+    with open(filename , "a") as output , open(parameterfile , "r") as params:
+
+        parameters = pandas.read_csv(params , index_col=0)
+        print(parameters)
+
+        out_array = []
+        df = pandas.DataFrame(out_array)
+        df.to_csv(output)
+    return True
+
+#mockData("parameters.csv" , 100)
+
+print(integer("a",0,304))
+print(factor("a",("red","yellow","blue","green")))
+print(string("a",200))
