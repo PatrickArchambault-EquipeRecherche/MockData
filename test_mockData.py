608d1fa410aefeda937076f79081eae88ec6003a
William Witteman <william.witteman.cisssca@ssss.gouv.qc.ca>
2021-11-04 11:01:40 -0400

    Basic structure working...

--- test_mockData.py
+++ test_mockData.py
@@ -5,4 +5,16 @@
 import pytest
 import mockData
 
-print(mockData.fakeIt("boolean"))
+def test_fakeIt():
+    errors = []
+
+    valueToTest = mockData.fakeIt("boolean")
+
+    if valueToTest == 1:
+        pass
+    elif valueToTest == 0:
+        pass
+    else:
+        errors.append("fakeIt('boolean') returns a bad value: %s".format(str(valueToTest)))
+
+    assert not errors, "Found errors:\n{}".format("\n".join(errors))
